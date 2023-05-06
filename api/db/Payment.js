1ab68b7dbd2dfc64c273de4ad2e0f32386e58412
Xavier Mukodi <xaviermukodi@gmail.com>
2023-05-06 10:44:08 +0200

    removed Payments.ref_code column

--- api/db/Payment.js
+++ api/db/Payment.js
@@ -1,16 +1,10 @@
 
 
 const { Model, DataTypes } = require("sequelize");
-const casual = require('casual');
 
 module.exports = class Payment extends Model {
    static init(sequelize) {
       super.init({
-         ref_code: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            defaultValue: () => casual.uuid,
-         },
          amount: {
             type: DataTypes.DOUBLE,
             allowNull: false,
